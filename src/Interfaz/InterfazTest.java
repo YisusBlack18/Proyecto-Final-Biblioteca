a810ed94826d8249de2067af48e0dd17f46bcd51
AkiroMiami <123915508+AkiroMiami@users.noreply.github.com>
2023-11-27 00:24:15 -0700

    Test ou llea

---
+++ src/Interfaz/InterfazTest.java
@@ -0,0 +1,75 @@
+package Interfaz;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.PrintStream;
+
+import static org.junit.Assert.assertSame;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import Interfaz.Interfaz;
+import Interfaz.UsuarioImpl;
+class InterfazTest {
+
+    private Interfaz interfaz;
+    private ByteArrayOutputStream outputStream;
+    private ByteArrayInputStream inputStream;
+
+    @BeforeEach
+    void setUp() {
+        interfaz = new Interfaz();
+        outputStream = new ByteArrayOutputStream();
+    }
+
+    @Test
+    void menuPrincipal() {
+        setInputStream("1\n");
+        System.setOut(new PrintStream(outputStream));
+        interfaz.menuPrincipal();
+        assertEquals("Bienvenido a la librería\n1. Iniciar sesión\n2. Registrarse\n3. Salir\n", outputStream.toString());
+    }
+
+    @Test
+    void menuAdmin() {
+        setInputStream("1\n");
+        System.setOut(new PrintStream(outputStream));
+        UsuarioImpl usuario = new UsuarioImpl("John", "Doe", "john.doe@example.com", "password", "admin");
+        interfaz.menuAdmin(usuario);
+        assertEquals("Bienvenido John Doe\n1. Administrar libros\n2. Administrar préstamos\n3. Administrar usuarios\n4. Salir\n", outputStream.toString());
+    }
+    @Test
+    void menuLibros() {
+        setInputStream("1\n");
+        System.setOut(new PrintStream(outputStream));
+        interfaz.menuLibros();
+        assertEquals("1. Agregar libro\n2. Eliminar libro\n3. Listar libros\n4. Salir\n", outputStream.toString());
+    }
+    @Test
+    void menuPrestamos() {
+        setInputStream("1\n");
+        System.setOut(new PrintStream(outputStream));
+        interfaz.menuPrestamos();
+        assertEquals("1. Prestar libro\n2. Devolver libro\n3. Listar préstamos\n4. Salir\n", outputStream.toString());
+    }
+    @Test
+    void menuUsuarios() {
+        setInputStream("1\n");
+        System.setOut(new PrintStream(outputStream));
+        interfaz.menuUsuarios();
+        assertEquals("1. Agregar usuario\n2. Eliminar usuario\n3. Listar usuarios\n4. Salir\n", outputStream.toString());
+    }
+    @Test
+    void menuUsuario() {
+        setInputStream("1\n");
+        System.setOut(new PrintStream(outputStream));
+        UsuarioImpl usuario = new UsuarioImpl("John", "Doe", "john.doe@example.com", "password", "admin");
+        interfaz.menuUsuario(usuario);
+        assertEquals("Bienvenido John Doe\n1. Listar libros\n2. Prestar libro\n3. Devolver libro\n4. Listar préstamos\n5. Salir\n", outputStream.toString());
+    }
+    private void setInputStream(String input) {
+        inputStream = new ByteArrayInputStream(input.getBytes());
+        System.setIn(inputStream);
+    }
+}
---
+++ src/Interfaz/LibroTest.java
@@ -0,0 +1,53 @@
+package Interfaz;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
+
+class LibroRepositoryTest {
+
+    private LibroRepository libroRepository;
+    private LibroFactory libroFactory;
+
+    @BeforeEach
+    void setUp() {
+        libroRepository = new LibroRepositoryImpl();
+        libroFactory = new LibroFactoryImpl();
+    }
+
+    @Test
+    void testAgregarYObtenerLibro() {
+        Libro libro = libroFactory.crearLibro("El Señor de los Anillos", "J.R.R. Tolkien", 1954);
+        libroRepository.agregarLibro(libro);
+
+        Libro libroObtenido = libroRepository.obtenerLibro(libro.isbn());
+
+        assertEquals(libro, libroObtenido);
+    }
+
+    @Test
+    void testEliminarLibro() {
+        Libro libro = libroFactory.crearLibro("Harry Potter", "J.K. Rowling", 1997);
+        libroRepository.agregarLibro(libro);
+
+        libroRepository.eliminarLibro(libro);
+
+        assertNull(libroRepository.obtenerLibro(libro.isbn()));
+    }
+
+    @Test
+    void testObtenerLibros() {
+        Libro libro1 = libroFactory.crearLibro("1984", "George Orwell", 1949);
+        Libro libro2 = libroFactory.crearLibro("Cien años de soledad", "Gabriel García Márquez", 1967);
+
+        libroRepository.agregarLibro(libro1);
+        libroRepository.agregarLibro(libro2);
+
+        List<Libro> libros = libroRepository.obtenerLibros();
+
+        assertEquals(2, libros.size());
+        assertTrue(libros.contains(libro1));
+        assertTrue(libros.contains(libro2));
+    }
+}
